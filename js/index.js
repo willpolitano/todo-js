0a6baf5d2fc4de873c4936d91c336b6f3e86a67e
Felipe Ferreira <sistema3@yescert.com.br>
2020-11-18 13:57:36 -0300

    estrutura modificada

--- js/index.js
+++ js/index.js
@@ -1,36 +1,14 @@
 const listTodo = document.querySelector('.todo-list')
 
 let inputNewTodo = document.querySelector('.new-todo')
-let idTemplate = 1
+let idLine = 1
 
 inputNewTodo.addEventListener('keyup', function(event) {
 
-    let valeuNewTodo = inputNewTodo.value.trim()
+    let valueNewTodo = inputNewTodo.value.trim()
 
-    if(event.keyCode === 13 && valeuNewTodo) {
-        newTodo(valeuNewTodo)
+    if(event.keyCode === 13 && valueNewTodo) {
+        newTodo(valueNewTodo)
     }
+
 })
-
-function newTodo(value) {
-    
-    let template = `<li data-id="${idTemplate}" class="">
-                        <div class="view">
-                            <input class="toggle" type="checkbox">
-                            <label> ${value} </label>
-                            <button class="destroy" onclick="destroyTodo(${idTemplate})"></button>
-                        </div>
-                    </li>`
-
-    listTodo.insertAdjacentHTML('beforeend', template)
-    
-    inputNewTodo.value = ''
-
-    idTemplate++
-}
-
-function destroyTodo (id) {
-    document.querySelector("[data-id='" + id + "']").remove()
-}
-
-
