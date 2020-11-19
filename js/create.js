6c349c0f5dcb5bcafee0e552a957a0a9d08baba8
willpolitano <willianpolitano@gmail.com>
2020-11-19 09:35:41 -0300

    completar tarefa

--- js/create.js
+++ js/create.js
@@ -2,7 +2,7 @@ function newTodo(value) {
     
     let line = `<li data-id="${idLine}" class="">
                     <div class="view">
-                        <input class="toggle" type="checkbox">
+                        <input class="toggle" type="checkbox" onClick="completeTodo(${idLine})">
                         <label> ${value} </label>
                         <button class="destroy" onClick="destroyTodo(${idLine})"></button>
                     </div>
--- js/index.js
+++ js/index.js
@@ -3,12 +3,21 @@ const listTodo = document.querySelector('.todo-list')
 let inputNewTodo = document.querySelector('.new-todo')
 let idLine = 1
 
-inputNewTodo.addEventListener('keyup', function(event) {
+inputNewTodo.addEventListener('keyup', function(e) {
 
     let valueNewTodo = inputNewTodo.value.trim()
 
-    if ( event.keyCode === 13 && valueNewTodo ) {
+    if ( e.keyCode === 13 && valueNewTodo )
         newTodo(valueNewTodo)
-    }
-
 })
+
+function completeTodo(id) {
+
+    let lineCompleted = document.querySelector("[data-id='" + id + "']")
+    let checkBox = lineCompleted.querySelector('.view .toggle')
+
+    if( checkBox.checked == true ) 
+        lineCompleted.classList.add('completed');
+    else 
+        lineCompleted.classList.remove('completed');
+}
