e09de6a9b27d3e8cd0268a2d50cb1143f0a9518b
willpolitano <willianpolitano@gmail.com>
2020-11-19 13:28:18 -0300

    links all, active e completed

--- js/index.js
+++ js/index.js
@@ -1,5 +1,6 @@
 const listTodo = document.querySelector('.todo-list')
 const checkBox = document.querySelector('.toggle-all')
+
 let inputNewTodo = document.querySelector('.new-todo')
 let idLine = 1
 
@@ -15,18 +16,25 @@ function completeTodo(id) {
 
     let lineComplete = document.querySelector("[data-id='" + id + "']")
     let checkBoxLine = lineComplete.querySelector('.view .toggle')
+    let lineAll = document.querySelectorAll('[data-id]')
+    let selected = document.querySelector('.selected')
+    let action = selected.id
 
     if( checkBoxLine.checked == true )  
         lineComplete.classList.add('completed')
     else 
         lineComplete.classList.remove('completed')
         checkBox.checked = false
+
+    see(lineAll, action)
     
 }
 
 function completeAll() {
     let lineAll = document.querySelectorAll('[data-id]')
     let checkBoxAll = document.querySelectorAll('.toggle')
+    let selected = document.querySelector('.selected')
+    let action = selected.id
 
     if( checkBox.checked == true ) 
 
@@ -41,4 +49,62 @@ function completeAll() {
             lineAll[i].classList.remove('completed')
             checkBoxAll[i].checked = false;
         }
+
+    see(lineAll, action)
+}
+
+function selectTodo(elem, e) {
+    
+    let lineAll = document.querySelectorAll('[data-id]')
+    let selected = document.querySelector('.selected')
+    let action = elem.id
+    
+    e.preventDefault()
+
+    selected.classList.remove('selected')
+    elem.classList.add('selected')
+
+    see (lineAll, action) 
+}
+
+function see(lineAll, action) {
+
+    for (i = 0; i < lineAll.length; i++) {
+ 
+        switch ( action ) {
+            case 'all' :
+
+                blockLine(lineAll[i])
+
+            break
+
+            case 'active' : 
+
+                if (lineAll[i].className === 'completed') 
+                    noneLine(lineAll[i])
+                else 
+                    blockLine(lineAll[i])
+
+            break
+
+            case 'completed' : 
+
+                if (lineAll[i].className !== 'completed')
+                    noneLine(lineAll[i])
+                else
+                    blockLine(lineAll[i])
+
+            break
+        }
+    }
+}
+
+function blockLine(line) {
+    
+    line.style.display = "block"
+}
+
+function noneLine(line) {
+    
+    line.style.display = "none"
 }
